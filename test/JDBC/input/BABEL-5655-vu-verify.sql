2c416f2b563e7394a0fd8e1d82b863430837b965
Rohit Bhagat <rohitbgt@amazon.com>
2025-03-11 19:47:14 +0000

    update tests
    Signed-off-by: Rohit Bhagat <rohitbgt@amazon.com>

--- test/JDBC/input/BABEL-5655-vu-verify.sql
+++ test/JDBC/input/BABEL-5655-vu-verify.sql
@@ -23,6 +23,29 @@ select
     end;
 go
 
+declare @strDev float = 2.73;
+select 0.00 as a, ROUND(@strDev, 2) as b into babel_5655_temp_t1
+go
+
+declare @strDev float = 2.73;
+select
+    case
+        when 2 = 3 then 0.00
+        else ROUND(@strDev, 2)
+    end as a
+into babel_5655_temp_t2
+go
+
+select TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, NUMERIC_PRECISION, NUMERIC_PRECISION_RADIX, NUMERIC_SCALE
+from information_schema.columns 
+where TABLE_NAME = 'babel_5655_temp_t1' order by COLUMN_NAME
+go
+
+select TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, NUMERIC_PRECISION, NUMERIC_PRECISION_RADIX, NUMERIC_SCALE
+from information_schema.columns 
+where TABLE_NAME = 'babel_5655_temp_t2' order by COLUMN_NAME
+go
+
 drop table babel_5655_t1
 go
 
